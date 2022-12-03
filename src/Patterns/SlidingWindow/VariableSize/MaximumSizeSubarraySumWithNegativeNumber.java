e2d2a6f702337ad56288e50717ea3f9a9ef61122
Avtar-Code-Farm <avtarsohi11@gmail.com>
2022-12-03 09:48:14 -0800

    Simplify Max sum subarray with negative numbers

--- src/Patterns/SlidingWindow/VariableSize/MaximumSizeSubarraySumWithNegativeNumber.java
+++ src/Patterns/SlidingWindow/VariableSize/MaximumSizeSubarraySumWithNegativeNumber.java
@@ -10,6 +10,47 @@ public class MaximumSizeSubarraySumWithNegativeNumber {
         int[] nums = new int[] {5,1,2,-3,3,-1,2,4};
         int res = maxSubArrayWithNegativeNumberLen(target, nums);
         System.out.println("max subarray size " + res + " with sum " + target);
+        int res2 = getMaxLengthSubArrayWithKSum(nums, target);
+    }
+
+
+
+    public static int getMaxLengthSubArrayWithKSum(int[] nums, int k) {
+        if(nums.length == 0) return 0;
+
+        int start = 0;
+        int max = Integer.MIN_VALUE;
+        int curr_sum = 0;
+
+        // map to hold curr_sum - k value and its index
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int end = 0; end < nums.length; end++) {
+            // keep adding values of the array into the curr_sum
+            curr_sum += nums[end];
+
+            // it means we got the sum from the index 0 and that is the max sum for given time.
+            if(curr_sum == k) max = end + 1;
+
+            // we will use the diff to find the index in the map if it exists. if it exists it means sum K also exists.
+            int diff = curr_sum - k;
+
+            if(map.containsKey(diff)) {
+                // it gives us the index of currsum-k
+                int index = map.get(diff);
+                max = Math.max(max, end - index);
+
+                // printing values.
+                System.out.println("New ");
+                for(int p = index + 1; p <= end; p++) {
+                    System.out.print( " " + nums[p] + " ");
+                }
+            }
+
+            map.putIfAbsent(curr_sum, end);
+        }
+
+        return max;
     }
 
     private static int maxSubArrayWithNegativeNumberLen(int k, int[] nums) {
@@ -40,6 +81,5 @@ public class MaximumSizeSubarraySumWithNegativeNumber {
         }
         // don't forget to check if there is such a subarray that meet the condition
         return maxLen == Integer.MIN_VALUE ? 0 : maxLen;
-
     }
 }
